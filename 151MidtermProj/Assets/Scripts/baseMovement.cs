bd19f359f85409c770ac947388278f1d136e55bb
grammb <108758537+grammb@users.noreply.github.com>
2023-05-01 21:18:10 -0700

    basic movement script in

---
+++ 151MidtermProj/Assets/Scripts/baseMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//************** use UnityOSC namespace...
+using UnityOSC;
+//*************
+
+public class baseMovement : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //get the rigidbody component
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        //Debug.Log(rb.position.x);
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+}
